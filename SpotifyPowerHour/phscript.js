87a227ca2fd1e15ae68dfacb32d34786d7dd3fcd
xps <xps@boone.boone>
2020-04-10 21:06:27 -0500

    123

--- SpotifyPowerHour/phscript.js
+++ SpotifyPowerHour/phscript.js
@@ -112,33 +112,32 @@ function start() {
 
         player.getCurrentState().then(state => {
             if (state) {
-                play(data.device_id);
-            } else {
-
-                // Error handling
-                player.on('initialization_error', e => console.error(e));
-                player.on('authentication_error', e => console.error(e));
-                player.on('account_error', e => console.error(e));
-                player.on('playback_error', e => console.error(e));
-
-                // Playback status updates
-                player.on('player_state_changed', state => {
-                    console.log(state)
-                    $('#current-track').attr('src', state.track_window.current_track.album.images[0].url);
-                    $('#current-track-name').text(state.track_window.current_track.name);
-                });
-
-                // Ready
-                player.on('ready', data => {
-                    console.log('Ready with Device ID', data.device_id);
-
-                    // Play a track using our new device ID
-                    play(data.device_id);
-                });
-
-                // Connect to the player!
-                player.connect();
+                player.removeListener('ready');
             }
+
+            // Error handling
+            player.on('initialization_error', e => console.error(e));
+            player.on('authentication_error', e => console.error(e));
+            player.on('account_error', e => console.error(e));
+            player.on('playback_error', e => console.error(e));
+
+            // Playback status updates
+            player.on('player_state_changed', state => {
+                console.log(state)
+                $('#current-track').attr('src', state.track_window.current_track.album.images[0].url);
+                $('#current-track-name').text(state.track_window.current_track.name);
+            });
+
+            // Ready
+            player.on('ready', data => {
+                console.log('Ready with Device ID', data.device_id);
+
+                // Play a track using our new device ID
+                play(data.device_id);
+            });
+
+            // Connect to the player!
+            player.connect();
         })
     } else {
         alert('Please select a playlist');
